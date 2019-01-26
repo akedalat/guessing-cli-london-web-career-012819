a48b5bde4bc9a5694dd50ab13facfb2c1a31cf59
Kian Edalat <akedalat@gmail.com>
2019-01-26 18:40:33 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,13 +5,13 @@ while input
   puts "Guess a number between 1 and 6."
   input = gets.chomp
   randon_number = rand(1..6).to_s
-if input == "exit"
-  puts "Goodbye!"
-  return 0
-elsif input == randon_number
-  puts "You guessed the correct number!"
-else
-  puts "The computer guessed #{randon_number}"
-end
-end
+    if input == "exit"
+      puts "Goodbye!"
+      return 0
+    elsif input == randon_number
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{randon_number}"
+    end
+  end
 end
