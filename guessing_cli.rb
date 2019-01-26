03a9f167af4d3203dbd23f1c06c947499099caa8
Kian Edalat <akedalat@gmail.com>
2019-01-26 18:41:49 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
 input = ""
 while input
   puts "Guess a number between 1 and 6."
-  input = gets.chomp
+  input = gets.downcase.chomp
   randon_number = rand(1..6).to_s
     if input == "exit"
       puts "Goodbye!"
