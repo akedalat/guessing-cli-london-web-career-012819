832090faa20940f1eac3fcc40f0179ac3cbd1c75
Kian Edalat <akedalat@gmail.com>
2019-01-26 18:35:58 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,12 +6,12 @@ while input
   input = gets.chomp
   randon_number = rand(1..6).to_s
 if input == "exit"
-  puts "Goodbye!"
+  return "Goodbye!"
 elsif input.to_i == randon_number
   binding.pry
-  return "You guessed the correct number!"
+  puts "You guessed the correct number!"
 else
-  return "The computer guessed #{randon_number}"
+  puts "The computer guessed #{randon_number}"
 end
 end
 end
